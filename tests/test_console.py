21fd3d962883789b516f04871b5e42c979abb4d5
NewtonMutugi <newtonmut197@gmail.com>
2023-05-28 01:18:52 +0300

    Added test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Unittest for console.py"""
+
+import unittest
+from console import HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+    """Tests for console.py"""
+
+    def test_create(self):
+        """Test create command"""
+        pass
+
+    def test_show(self):
+        """Test show command"""
+        pass
+
+    def test_destroy(self):
+        """Test destroy command"""
+        pass
+
+    def test_all(self):
+        """Test all command"""
+        pass
+
+    def test_update(self):
+        """Test update command"""
+        pass
+
+    def test_quit(self):
+        """Test quit command"""
+        pass
+
+    def test_EOF(self):
+        """Test EOF command"""
+        pass
+
+    def test_emptyline(self):
+        """Test emptyline command"""
+        pass
+
+    def test_help(self):
+        """Test help command"""
+        pass
+
+    def test_count(self):
+        """Test count command"""
+        pass
+
+    def test_base_model(self):
+        """Test base model command"""
+        pass
+
+    def test_user(self):
+        """Test user command"""
+        pass
+
+    def test_state(self):
+        """Test state command"""
+        pass
+
+    def test_city(self):
+        """Test city command"""
+        pass
+
+    def test_amenity(self):
+        """Test amenity command"""
+        pass
+
+    def test_place(self):
+        """Test place command"""
+        pass
+
+    def test_review(self):
+        """Test review command"""
+        pass
